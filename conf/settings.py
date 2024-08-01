849f47193d807fe2c109e71d1f52a0d69f91df8e
ZiyodilloYigitaliyev <ziyodilloyigitaliyev365@gmail.com>
2024-08-01 09:29:44 -0700

    new updates

--- conf/settings.py
+++ conf/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +37,22 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dorilar',
+    'user',
+    #installed apps:
+    'rest_framework'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -67,7 +82,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'conf.wsgi.application'
+AWSGI_APPLICATION = 'conf.wsgi.application'
 
 
 # Database
@@ -115,7 +130,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'Media'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- conf/urls.py
+++ conf/urls.py
@@ -14,9 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 
-urlpatterns = [
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from django.conf.urls.static import static
+from django.conf import settings
+from dorilar.views import *
+
+router = DefaultRouter()
+router.register(r'dorilar', DoriViewSet)
+router.register(r'buyurtmalar', BuyurtmaViewSet)
+
+
+urlpatterns = ([
     path('admin/', admin.site.urls),
-]
+    path('api/', include(router.urls)),
+ ])
+
